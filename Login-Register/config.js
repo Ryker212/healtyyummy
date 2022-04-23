8134eee7ae6683e43617bf79c724cd624ca0c25b
paiizxqh <aonkunya7@gmail.com>
2022-04-23 21:09:36 +0700

    21.09

--- Login-Register/config.js
+++ Login-Register/config.js
@@ -1,11 +1,12 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA8EzeDcC1eWSYeoGZRyXpupi5ToZhn1Ew",
-  authDomain: "g1-zes.firebaseapp.com",
-  projectId: "g1-zes",
-  storageBucket: "g1-zes.appspot.com",
-  messagingSenderId: "166111315060",
-  appId: "1:166111315060:web:3e53d459e6a016f0e3fd21",
-  measurementId: "G-F1SCBC03LR"
+const firebaseConfig ={
+  apiKey: "AIzaSyBHqM14Ai6Kriexr29sy7ZXuIR7eOjvKPU",
+  authDomain: "testchat-c44d8.firebaseapp.com",
+  databaseURL: "https://testchat-c44d8-default-rtdb.firebaseio.com/",
+  projectId: "testchat-c44d8",
+  storageBucket: "testchat-c44d8.appspot.com",
+  messagingSenderId: "780474019289",
+  appId: "1:780474019289:web:79077b454e94cdc6737dc8",
+  measurementId: "G-70H6096HFQ"
 };
 firebase.initializeApp(firebaseConfig)
