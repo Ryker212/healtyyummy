bca1a92510acae41f685087043ffe35528f5bbe1
healtyyummy <103986795+healtyyummy@users.noreply.github.com>
2022-04-23 10:23:42 +0700

    Add files via upload

--- Chatlive.js
+++ Chatlive.js
@@ -1,28 +1,51 @@
-const firebaseConfig ={
-    apiKey: "AIzaSyBHqM14Ai6Kriexr29sy7ZXuIR7eOjvKPU",
-    authDomain: "testchat-c44d8.firebaseapp.com",
-    databaseURL: "https://testchat-c44d8-default-rtdb.firebaseio.com",
-    projectId: "testchat-c44d8",
-    storageBucket: "testchat-c44d8.appspot.com",
-    messagingSenderId: "780474019289",
-    appId: "1:780474019289:web:79077b454e94cdc6737dc8",
-    measurementId: "G-70H6096HFQ"
-};
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-  
-  
-    
 
-var name = localStorage.getItem("name");
+const db = firebase.firestore()
+
+
+/*localStorage.setItem("email", email);*/
+
+ 
+
+
+
+  
+  var email = localStorage.getItem("email");
+  var namer;
+  console.log("email",email);
+  
+  db.collection("User").doc(email).get()
+  .then(function (doc) {
+    if(doc.exists){
+    namer = doc.data().Username;      
+    console.log("name",namer)
+    localStorage.setItem("namer", namer)
+    var namer = localStorage.getItem("namer");
+
+    
+    }
+    else{
+      console.log("does not exist")
+      alert("Successfully")
+    }
+  })
+  .catch(function(error){
+
+    console.log("error",error)
+  })
+  
+
+
+
 function send()
 {
-    var name = localStorage.getItem("name");
+    
+    
+    var namer = localStorage.getItem("namer");
     var msg =document.getElementById('msg_text').value;
     if(msg !=""){
         firebase.database().ref("messages").push({
             msg:msg,
-            sender: name
+            sender: namer
         }).then(function(){
             document.getElementById('msg_text').value="";
         
@@ -33,22 +56,42 @@ function back()
 {
   firebase.database().ref("messages").remove();
 
-    window.location.replace("index.html");
+    window.location.replace("menu.html");
 }
 let ref= firebase.database().ref("messages").on('child_added' , function(snapshot){
     var username = snapshot.val().sender;
     var msg =snapshot.val().msg;
     var html ="";
-    let name = localStorage.getItem("name");
-    if(username == name){
-        html += "<div class='message_me' align='right'><p class='user'>" +username +"</p><p class='msg_me'>"+msg+"</p</div>";
-        document.getElementById("box_messages").innerHTML += html;
+    let namer = localStorage.getItem("namer");
+    
+    if (username == 'Nutrian')
+    {
         
-
+        if(username == namer){
+            
+            html += "<div class='message_me' align='right'><p class='user'>" +username +"</p><p class='msg_megod'>"+msg+"</p</div>";
+            document.getElementById("box_messages").innerHTML += html;
+            
+    
+        }
+        else{
+            html += "<div class='message_user' align='left'><p class='user'>" +username +"</p><p class='msg_usergod'>"+msg+"</p</div>";
+            document.getElementById("box_messages").innerHTML += html;
+            
+        }
+        
     }
     else{
-        html += "<div class='message_user' align='left'><p class='user'>" +username +"</p><p class='msg_user'>"+msg+"</p</div>";
-        document.getElementById("box_messages").innerHTML += html;
+        if(username == namer){
+            html += "<div class='message_me' align='right'><p class='user'>" +username +"</p><p class='msg_me'>"+msg+"</p</div>";
+            document.getElementById("box_messages").innerHTML += html;
+            
+    
+        }
+        else{
+            html += "<div class='message_user' align='left'><p class='user'>" +username +"</p><p class='msg_user'>"+msg+"</p</div>";
+            document.getElementById("box_messages").innerHTML += html;
+    }
         
     }
 })
