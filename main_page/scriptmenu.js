1b773074ba091fa8226a62cc3c93a2919cc29427
Ryker212 <103985410+Ryker212@users.noreply.github.com>
2022-04-23 15:42:29 +0700

    Add files via upload

---
+++ main_page/scriptmenu.js
@@ -0,0 +1,12 @@
+$("#search").keyup(function() {
+    var val = $.trim(this.value);
+    var header = "menu"
+          if (val === "") {
+              $('img').show();
+          }
+          else {
+              $('img').hide();
+              $("img[alt*=" + val +"]").show();
+              $("img[alt*=" + header +"]").show();
+          }
+    });
